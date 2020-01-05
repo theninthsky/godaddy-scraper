b14d8745315179987eb6961cdcaeac4843b1b4a7
Almog Gabay <theninesky@gmail.com>
2020-01-05 23:58:38 +0200

    save all domains to a csv file instead of txt

--- domain-generator.js
+++ domain-generator.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 const names = [];
-const adjectives = JSON.parse(fs.readFileSync('adjectives.txt', 'utf-8'));
-const nouns = JSON.parse(fs.readFileSync('nouns.txt', 'utf-8'));
+const adjectives = JSON.parse(fs.readFileSync('english-vocabulary/adjectives.txt', 'utf-8'));
+const nouns = JSON.parse(fs.readFileSync('english-vocabulary/nouns.txt', 'utf-8'));
 
 for (const adjective of adjectives) {
     for (const noun of nouns) {
--- scraper.js
+++ scraper.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const { question } = require('readline-sync');
 const puppeteer = require('puppeteer');
 
+const currencyList = '$¢£¤¥֏؋߾߿৲৳৻૱௹฿៛₠₡₢₣₤₥₦₧₨₩₪₫€₭₮₯₰₱₲₳₴₵₶₷₸₹₺₻₼₽₾₿꠸﷼﹩＄￠￡￥￦𑿝𑿞𑿟𑿠𞋿𞲰';
+const selector = '.exact-body-result';
+
 console.log('\n*** GoDaddy Scraper ***\n')
 
 const [fromIndex, toIndex] = [+question('Check from index: '), +question('To index: ')];
@@ -20,17 +23,21 @@ if (numOfThreads > numOfDomains) {
 const indexesPerThread = ~~(numOfDomains / numOfThreads);
 const remainder = numOfDomains % numOfThreads;
 
-const domainNames = fs.readFileSync('./domain-generator/names.txt', 'utf-8').split`,`;
+const domainNames = fs.readFileSync('english-names.txt', 'utf-8').split`,`;
 const domains = [];
 
 console.log('');
 
 const fetchData = async (page, url, index) => {
     await page.goto(url);
-    const selector = '.exact-body-result';
     await page.waitForSelector(selector, { timeout: 10000 });
     const result = await page.evaluate(selector => document.querySelector(selector).textContent, selector);
-    domains.push(`[${index}] ${result.split` `[0]}: ${(result.match(/₪[\d,.]+|taken/))[0]}`);
+    domains.push(`\
+        ${index},\
+        ${result.split` `[0]},\
+        ${(result.match(new RegExp(`(?<=[${currencyList}])[\\d,.]+|taken`)))[0].replace(',', '')},\
+        ${(result.match(new RegExp(`[${currencyList}]`)) || ['-'])[0]}\
+    `);
 };
 
 const thread = async (page, startInd, endInd) => {
@@ -56,11 +63,10 @@ const thread = async (page, startInd, endInd) => {
 
     await Promise.all(threads);
     browser.close();
-    fs.appendFile(
-        'domains.txt',
-        domains.sort((a, b) => +a.match(/\d+/)[0] - +b.match(/\d+/)[0]).join`\n\n` + '\n\n',
-        () => { }
-    );
+    if (!fs.existsSync('domains.csv')) {
+        fs.writeFileSync('domains.csv', 'INDEX,DOMAIN,PRICE,CURRENCY\n');
+    }
+    fs.appendFile('domains.csv', domains.sort((a, b) => +a.match(/\d+/)[0] - +b.match(/\d+/)[0]).join`\n` + '\n', () => { });
 
-    console.log(`\nCompleted in ${((Date.now() - time) / 1000).toFixed(2)}s. Logs are saved to file "domains.txt".\n`);
+    console.log(`\nCompleted in ${((Date.now() - time) / 1000).toFixed(2)}s. Logs are saved to file "domains.csv".\n`);
 })();
